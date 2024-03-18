5aa6d158a813a0f5f2eaa3c3e73a1c2ecf1a4c6a
Gmclaro <gui@ua.pt>
2024-03-18 22:11:21 +0000

    penis pushado

--- yellowfy/lib/services/remote_service.dart
+++ yellowfy/lib/services/remote_service.dart
@@ -2,6 +2,7 @@ import 'package:http/http.dart' as http;
 import 'package:yellowfy/models/post.dart';
 
 class RemoteService {
+  var client = http.Client();
   Future<List<Announcement>?> getAnnouncements() async {
     var client = http.Client();
     var uri = Uri.parse('https://yellowfy.herokuapp.com/announcements');
