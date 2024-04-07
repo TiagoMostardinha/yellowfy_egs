1274bc5cbadb4e077ace56193fcba18641e0650e
Edgar Sousa <edgarsousa@ua.pt>
2024-04-07 18:40:32 +0100

    merge to main branch

---
+++ models.py
@@ -0,0 +1,17 @@
+# models.py
+
+from pydantic import BaseModel
+from typing import Optional
+
+class Appointment(BaseModel):
+    id: int
+    service_type: str
+    date_time: str
+    client_id: int
+    client_name: str
+    contractor_name: str
+    contractor_contact: str
+    contractor_id: int  
+    duration: int
+    details: Optional[str] = None
+
