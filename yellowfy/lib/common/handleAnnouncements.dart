2b5e4e756f9c05e6789744c7bce68324e2a69dc4
Gmclaro <gui@ua.pt>
2024-04-08 22:32:22 +0100

    api do mostardinha conectada

--- yellowfy/lib/common/handleAnnouncements.dart
+++ yellowfy/lib/common/handleAnnouncements.dart
@@ -4,19 +4,28 @@ import 'dart:convert';
 
 class handleAnnouncements {
   Future<List<Announcement>> handleGetAnnouncements() async {
-    final response = await http.get(Uri.parse('http://172.24.24.1:8080/v0/'));
+    final response = await http.get(Uri.parse("http://10.0.2.2:8080/v0/"));
     if (response.statusCode == 200) {
       List<Announcement> announcements = [];
       var data = jsonDecode(response.body);
+      print(data);
       for (var i in data) {
+        print("HEY");
+        var id = i['id'] ?? '';
+        var userId = i['userID'] ?? '';
+        var category = i['category'] ?? '';
+        var description = i['description'] ?? '';
+        var latitude = double.tryParse(i['latitude'] ?? '') ?? 0.0;
+        var longitude = double.tryParse(i['longitude'] ?? '') ?? 0.0;
+
         announcements.add(Announcement(
-          id: i['id'],
-          userId: i['userID'],
-          category: i['category'],
-          description: i['description'],
+          id: id,
+          userId: userId,
+          category: category,
+          description: description,
           coordinates: Coordinate(
-            latitude: i['location']['lat'],
-            longitude: i['location']['long'],
+            latitude: latitude,
+            longitude: longitude,
           ),
         ));
       }
