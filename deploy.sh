7cb2935ae11a297f69db0feb91e084c5ee40124b
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2024-05-15 14:23:04 +0100

    deploy announcements

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
 NAMESPACE="registry.deti/egs-yellowfy/"
-echo "Deploying in $HOST..."
+
+echo "Deleting previous deployment..."
+kubectl delete -f ./deployment/deployment.yaml
+
+echo "Deploying in $NAMESPACE..."
 
 echo "Building images..."
 echo "  auth"
@@ -12,4 +17,17 @@ docker buildx build --platform linux/amd64 --network=host -t $NAMESPACE/announce
 echo "  booking"
 docker buildx build --platform linux/amd64 --network=host -t $NAMESPACE/booking:v1 ./Booking
 
+echo "  nginx"
+docker buildx build --platform linux/amd64 --network=host -t $NAMESPACE/nginx:v1 -f ./deployment/Dockerfile.nginx ./deployment
+
 echo "Deploying..."
+docker push $NAMESPACE/auth:v1
+docker push $NAMESPACE/auth-db:v1
+docker push $NAMESPACE/announcements:v1
+docker push $NAMESPACE/booking:v1
+docker push $NAMESPACE/nginx:v1
+
+echo "Applying deployment in $NAMESPACE pod..."
+kubectl apply -f ./deployment/deployment.yaml
+
+echo "Done!"
