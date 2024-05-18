f255370408fce2d53dd44063463d9ce2b0608c76
Gmclaro <gui@ua.pt>
2024-05-18 14:24:17 +0100

    login

--- yellowfy/lib/login.dart
+++ yellowfy/lib/login.dart
@@ -1,9 +1,14 @@
+import 'dart:ffi';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:yellowfy/announcements.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:auth0_flutter/auth0_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -32,20 +37,74 @@ class LoginPageState extends State<LoginPage> {
         backgroundColor: Colors.yellowAccent[700],
       ),
       body: WebView(
-        initialUrl: _url,
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller = webViewController;
-        },
-        navigationDelegate: (NavigationRequest request) {
-          if (request.url.startsWith('_url')) {
-            final token = request.url.split('token=')[1];
-            _storage.write(key: 'token', value: token);
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        },
-      ),
+          initialUrl: _url,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller = webViewController;
+          },
+          onPageFinished: (url) async {
+            if (url != null && url.contains("callback")) {
+              final statusPage = await _controller.currentUrl();
+              if (statusPage != null && statusPage.contains("error")) {
+                Navigator.of(context).pop();
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      'Login failed. Please try again.',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    duration: Duration(seconds: 5),
+                  ),
+                );
+              } else {
+                final jsonString = await _controller
+                    .runJavascriptReturningResult('document.body.innerText');
+                final jsonData = jsonDecode(jsonString);
+
+                final res = jsonData.toString();
+                final out = res.split(" ");
+                final token = out[3]
+                    .replaceAll('"', '')
+                    .replaceAll('}', '')
+                    .replaceAll('\n', '');
+                debugPrint("Token -> " + token, wrapWidth: 1024);
+                await _storage.write(key: 'token', value: token);
+                Navigator.of(context).pop();
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => AnnouncementsPage(),
+                  ),
+                );
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      'Login successful!',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    duration: Duration(seconds: 5),
+                    backgroundColor: Colors.green,
+                  ),
+                );
+                final String url = 'http://10.0.2.2:5000/additional_info';
+                final Map<String, String> headers = {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json',
+                  'x-access-token': token,
+                };
+                final response =
+                    await http.post(Uri.parse(url), headers: headers);
+
+                if (response.statusCode == 200) {
+                  debugPrint("User logged in successfully", wrapWidth: 1024);
+                } else {
+                  debugPrint("User login failed : ${response.statusCode}",
+                      wrapWidth: 1024);
+                }
+              }
+            }
+          }),
     );
   }
 }
