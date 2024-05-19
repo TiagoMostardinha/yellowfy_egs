f9aa545db4b0659ab62b61fc383233e822d64826
Gmclaro <gui@ua.pt>
2024-05-19 17:17:55 +0100

    fds

--- yellowfy/lib/login.dart
+++ yellowfy/lib/login.dart
@@ -1,12 +1,9 @@
-import 'dart:ffi';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:yellowfy/announcements.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
